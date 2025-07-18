7758ec5a9208f8e94b7c7cb741ab6f43725b3294
Sumiku <devontedacosta1498@gmail.com>
2025-07-18 12:16:48 -0400

    Delete src/main directory

--- src/main/java/dev/kir/sync/entity/KillableEntity.java
+++
@@ -1,9 +0,0 @@
-package dev.kir.sync.entity;
-
-public interface KillableEntity {
-    default void onKillableEntityDeath() { }
-
-    default boolean updateKillableEntityPostDeath() {
-        return false;
-    }
-}
